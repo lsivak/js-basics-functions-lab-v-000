d0e6c4bd540a5484a6ac0b9f34ee08a5fe3d6e06
Lara Sanders <larasanders@gmail.com>
2018-05-16 20:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function distanceTravelledInFeet(someValue, endValue) {
   return distanceFromHqInBlocks(endValue)*264 - distanceFromHqInBlocks(someValue)*264
 }
 function calculatesFarePrice(start, destination) {
-  if(distanceTravelledInFeet(start, destination) > 400) {
+  if(distanceTravelledInFeet(start, destination) < 400) {
   return 0
 // } elsif (distanceTravelledInFeet(someValue, endValue) > 2000) {
 //   return (1600 - (distanceTravelledInFeet(someValue, endValue))*2
