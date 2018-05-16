912fb40df753e14bea4b33b6e35df282d94a3150
Lara Sanders <larasanders@gmail.com>
2018-05-16 19:53:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 function distanceFromHqInBlocks(someValue) {
-    if someValue >= 42
 const distance = 42 - someValue
-return distance
-} else {
-  const distance = someValue - 42
   return distance
-}
+
 }
